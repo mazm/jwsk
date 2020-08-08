fddba184823ce7d7c0ce7c09647b58e1f9b60235
yoon926 <roadyoon@gmail.com>
2020-08-09 03:11:19 +0900

    main, market, login

--- src/vendor_entries/vendor-compile.js
+++ src/vendor_entries/vendor-compile.js
@@ -2,5 +2,17 @@
 //
 // You can use any module syntax
 $(document).ready(() => {
-  $('.bxslider').bxSlider();
+  $('.bxslider').bxSlider({
+    controls: false,
+    auto: true,
+    pause: 5000,
+  });
+  // var burger = document.querySelector('.burger-container'),
+  //     header = document.querySelector('.header');
+  $('.burger-container').on('click', function() {
+      $('.header').toggleClass('menu-opened');
+  })
+  // burger.onclick = function() {
+  //   header.classList.toggle('menu-opened');
+  // };
 });
